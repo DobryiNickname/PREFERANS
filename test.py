c2b38672276af4b8d55830cf80cfaa6442038673
AnGeLo4ek <34009663+DobryiNickname@users.noreply.github.com>
2023-02-12 09:41:59 +0300

    [DP-999] Add trick dataframe

--- test.py
+++ test.py
@@ -1,31 +1,56 @@
+import pandas as pd
+from typing import List
+
+
 from utils import generate_hands_and_talon, put_card_on_table
 from bot import Bot
 
-hands = generate_hands_and_talon()
+class RoundPlay():
+    def __init__(self):
+        hands = generate_hands_and_talon()
 
-bot0 = Bot(0, "MAXIMISE", hand=hands["hand_1"])
-bot0.show_hand()
+        self.bot0 = Bot(0, "MAXIMISE", hand=hands["hand_1"])
+        self.bot0.show_hand()
 
-bot1 = Bot(1,"RANDOM", hand=hands["hand_2"])
-bot1.show_hand()
+        self.bot1 = Bot(1,"RANDOM", hand=hands["hand_2"])
+        self.bot1.show_hand()
 
-bot2 = Bot(2, "RANDOM", hand=hands["hand_3"])
-bot2.show_hand()
+        self.bot2 = Bot(2, "RANDOM", hand=hands["hand_3"])
+        self.bot2.show_hand()
 
-bot_id_mapping = {
-    0: bot0,
-    1: bot1,
-    2: bot2,
-}
+        self.bot_id_mapping = {
+            0: self.bot0,
+            1: self.bot1,
+            2: self.bot2,
+        }
 
-bot_order_mapping = {
-    "first bot turn": [0, 1, 2],
-    "second bot turn": [1, 2, 0],
-    "third bot turn": [2, 0, 1],
-}
+        self.bot_order_mapping = {
+            "first bot turn": [0, 1, 2],
+            "second bot turn": [1, 2, 0],
+            "third bot turn": [2, 0, 1],
+        }
 
-initial_bot_order = bot_order_mapping["first bot turn"]
-for _ in range(10):
-    print(f"Ходы до взятки -    {initial_bot_order:}")
-    initial_bot_order = put_card_on_table(bot_id_mapping, initial_bot_order, 1)
-    print(f"Ходы после взятки - {initial_bot_order:}")
+        self.result_table = {
+            "Bot_0_trick_score": 0,
+            "Bot_1_trick_score": 0,
+            "Bot_2_trick_score": 0,
+        }
+
+    def run(self):
+        initial_bot_order = self.bot_order_mapping["first bot turn"] # нужен итератор по кругу
+
+        for _ in range(10):
+            initial_bot_order = put_card_on_table(self.bot_id_mapping, initial_bot_order, 1) # нужен baseline для определения козыря
+            self.add_trick_to_table(initial_bot_order)
+
+        self.show_round_result()
+
+    def add_trick_to_table(self, bot_order_after_trick: List[int]):
+        self.result_table[f"Bot_{bot_order_after_trick[0]}_trick_score"] += 1
+
+    def show_round_result(self):
+        res_df = pd.DataFrame(self.result_table, index=[0])
+        print(res_df)
+
+round = RoundPlay()
+round.run()
